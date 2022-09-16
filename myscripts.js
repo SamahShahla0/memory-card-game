a9aa76824a5ed2cf64403c7cae5517bd927d52cf
Samah Shahla <samahch6@gmail.com>
2022-09-16 19:21:06 +0300

    scripting the algo

--- myscripts.js
+++ myscripts.js
@@ -1,11 +1,13 @@
 const cards = document.querySelectorAll('.memory-card');
+//setting a flag value for this card div to false, the flag variable indicates if its flipped or not
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard ;
+let secondCard ;
 
 function flipCard() {
-    //setting a flag value for this card div to false, the flag variable indicates if its flipped or not
-    let hasFlippedCard = false;
-    let firstCard;
-    let secondCard;
-
+    if (lockBoard) return;
+    if (this === firstCard) return;
     //adding the flip class to the card
     this.classList.add('flip');
     //checking if the card is not flipped 
@@ -16,9 +18,8 @@ function flipCard() {
         firstCard = this;
         return;
    }
-   
+
    secondCard = this;
-   hasFlippedCard = false;
 
    checkForMatch();
 }
@@ -33,13 +34,28 @@ function checkForMatch() {
 function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
+    if (this === firstCard) return;
 }
 
 function unflipCards() {
+    lockBoard = true;
     setTimeout(() => {
       firstCard.classList.remove('flip');
       secondCard.classList.remove('flip');
-    }, 1500);
+      resetBoard();
+    }, 1300);
 }
+
+function resetBoard() {
+    hasFlippedCard, lockBoard = false, false;
+    firstCard, secondCard = null, null;
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+      let ramdomPos = Math.floor(Math.random() * 6);
+      card.style.order = ramdomPos;
+    });
+})();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
