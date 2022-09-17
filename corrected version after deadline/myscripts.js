f1e1f37101508be5daa55881859f9f8774a14c6f
Samah Shahla <samahch6@gmail.com>
2022-09-17 06:01:40 +0300

    corrected the assignment

--- corrected version after deadline/myscripts.js
+++ corrected version after deadline/myscripts.js
@@ -1,51 +1,63 @@
 const cards = document.querySelectorAll('.memory-card');
-const memorygame = document.getElementsByClassName('memory-game');
-console.log(memorygame);
 
-let isFlipped = false;
+sessionStorage.setItem("score", 0);
+let score = parseInt(sessionStorage.getItem('score'));
+sessionStorage.setItem("flips", 0);
+let flips = parseInt(sessionStorage.getItem('flips'));
+
 let couple = [];
-console.log(couple);
+
+const scoreh2 = document.createElement('h2');
+scoreh2.innerText = `Score: ${sessionStorage.getItem('score')} , Flips: ${sessionStorage.getItem('flips')}`;
+scoreh2.style.textAlign = "center"; scoreh2.style.marginBottom = "10px"; scoreh2.style.fontSize = "36px"; 
+const referenceNode = document.querySelector('#welcome');
+referenceNode.after(scoreh2);
+//document.body.appendChild(scoreh2);
 
 function flipCard() {
-  if (isFlipped == false){
-    this.classList.add('flip');
-    console.log('flipping');
-    isFlipped = true;
-    couple.push(this);
-    console.log(couple);
-    return;
-  }
-  if(isFlipped){
-    return;
-  }
-  
   this.classList.add('flip');
-  console.log('flipping');
+  flips = flips + 1;
+  sessionStorage['flips'] = flips;
+  scoreh2.innerText = `Score: ${sessionStorage.getItem('score')} , Flips: ${sessionStorage.getItem('flips')}`;
   couple.push(this);
-  console.log(couple);
-  isFlipped = false;
-  checkForMatch();
+  if (couple.length == 2){
+    checkForMatch(couple);
+    couple = [];
+    return;
+  }
 
-}
+  /*const gameDiv = document.querySelector('game-div'); 
+  console.log(gameDiv);*/ 
+  //document.gameDiv.appendChild(scoreDiv); 
 
-function checkForMatch() {
-    //if () {
   
-    //}
-    unflipCards();
 }
 
-function unflipCards() {
+function checkForMatch(couple) {
+  
+    if (couple[0].classList.value === couple[1].classList.value) {
+      score = score + 1;
+      sessionStorage['score'] = score;
+      scoreh2.innerText = `Score: ${sessionStorage.getItem('score')} , Flips: ${sessionStorage.getItem('flips')}`;
+      setTimeout(() => {
+        couple[0].remove();
+        couple[1].remove();
+      }, 1500); 
+    }
+    else{
+      unflipCards(couple);
+    }
+}
+
+function unflipCards(couple) {
     setTimeout(() => {
-      //.classList.remove('flip');
-      //secondCard.classList.remove('flip');
-      resetBoard();
-    }, 1500);
+      couple[0].classList.remove('flip');
+      couple[1].classList.remove('flip');
+    }, 1000);
 }
 
-function resetBoard() {
-  isFlipped = false;
-}
+
+
 /////////////////////////////////////////////////////////////////////////////////
 //this will be executed on DOM load
 (function shuffle() {
